8a1c92870048f88d648bf3d637451f8d7a5f4f85
Tu usuario de GitHub aca <Tu email aca>
2023-03-11 17:31:43 -0500

    validar antes de mostrar los registros

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -15,11 +15,13 @@
         <div class="bg-gradient-to-r from-cyan-500 to-blue-500 opacity-75 rounded text-white pt-5 pb-20 px-5">
             <h3 class="text-center font-black text-3xl">Últimos Registros</h3>
             <div class="mt-5">
+            @if(!empty($registros))
                 @foreach ($registros as $registro)
                 <p class="">
                     {{ $registro->usuario->name }}
                 </p>
                 @endforeach
+            @endif
             </div>
         </div>
 
@@ -31,22 +33,26 @@
         <div class="bg-primario opacity-75 rounded text-white pt-5 pb-20 px-5">
             <h3 class="text-center font-black text-3xl">Eventos Con Menos Lugares Disponibles</h3>
             <div class="mt-5">
+            @if(!empty($menos_disponibles))
                 @foreach ($menos_disponibles as $evento)
                 <p class="bloque__texto">
                     {{ $evento->nombre . " - " . $evento->disponibles . ' Disponibles' }}
                 </p>
                 @endforeach
+            @endif
             </div>
         </div>
 
         <div class="bg-grisoscuro opacity-75 rounded text-white pt-5 pb-20 px-5">
             <h3 class="text-center font-black text-3xl">Eventos Con Más Lugares Disponibles</h3>
             <div class="mt-5">
+            @if(!empty($mas_disponibles))
                 @foreach ($mas_disponibles as $evento)
                 <p class="bloque__texto">
                     {{ $evento->nombre . " - " . $evento->disponibles . ' Disponibles' }}
                 </p>
                 @endforeach
+            @endif
             </div>
         </div>
     </div>
