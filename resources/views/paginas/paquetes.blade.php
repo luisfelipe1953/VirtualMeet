9801283158bf18dc74ded61be691d3cc968b8d1e
Tu usuario de GitHub aca <Tu email aca>
2023-06-06 21:41:32 -0500

    se agrego la ruta del deploy para las api

--- resources/views/paginas/paquetes.blade.php
+++ resources/views/paginas/paquetes.blade.php
@@ -83,7 +83,8 @@
                 return actions.order.capture().then(function(orderData) {
 
                     const datos = new FormData();
-                    datos.append('payment_id', orderData.purchase_units[0].payments.captures[0].id);
+                    datos.append('paquete_id', orderData.purchase_units[0].description);
+                    datos.append('pago_id', orderData.purchase_units[0].payments.captures[0].id);
 
                     fetch('/finish-registration/pay', {
                             method: 'POST',
@@ -129,9 +130,10 @@
             onApprove: function(data, actions) {
 
                 return actions.order.capture().then(function(orderData) {
-                    
+
                     const datos = new FormData();
-                    datos.append('payment_id', orderData.purchase_units[0].payments.captures[0].id);
+                    datos.append('paquete_id', orderData.purchase_units[0].description);
+                    datos.append('pago_id', orderData.purchase_units[0].payments.captures[0].id);
 
                     fetch('/finish-registration/pay', {
                             method: 'POST',
