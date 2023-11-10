5a8aaa1e6b54efd79afe3e5af149a73b198cff91
luisfelipe1953 <luisfelipefernandezbetancur@gmail.com>
2023-11-10 16:52:13 -0500

    se agrego docker

--- tests/Unit/UnitRecordRopositoryTest.php
+++ tests/Unit/UnitRecordRopositoryTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests\Unit;
+
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Event;
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
-
-
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/tags.js',
-                'resources/js/ponent.js',
-                'resources/js/horas.js',
-                'resources/js/tags.js',
-                'resources/js/slider.js',
-                'resources/js/mapa.js',
-                'resources/js/registro.js',
-                'resources/js/regalos.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/tags.js",
+                "resources/js/ponent.js",
+                "resources/js/horas.js",
+                "resources/js/tags.js",
+                "resources/js/slider.js",
+                "resources/js/mapa.js",
+                "resources/js/registro.js",
+                "resources/js/regalos.js",
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**'
-            ],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
+    server: {
+        host: "0.0.0.0",
+        port: 5200,
+        hmr: {
+            host: "localhost",
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
-
